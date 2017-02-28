66df24938b89b3e4db6c8904d4db1640919c19ab
Zayn Rahman <zaynrahn@gmail.com>
2017-02-28 06:37:40 +0000

    Replaced quote submission page with modal

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,10 +3,6 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
 
-  def new
-    @quote =Quote.new
-  end
-
   def create
     @quote = Quote.create(quote_params)
     if @quote.invalid?
@@ -15,14 +11,12 @@ class QuotesController < ApplicationController
     redirect_to root_path
   end
 
+  def about
+  end
+
   private
 
   def quote_params
     params.require(:quote).permit(:saying, :author)
   end
-
-  def about
-    
-  end
-
 end
